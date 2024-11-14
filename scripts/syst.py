2ed1cadc5f2fed98bb2c0906e3b0340352a267eb
Andrey Pozdnyakov <Andrey.Pozdnyakov@cern.ch>
2024-11-14 17:38:30 +0100

    adding some systematics

--- scripts/syst.py
+++ scripts/syst.py
@@ -1,18 +1,26 @@
 import CombineHarvester.CombineTools.ch as ch
 
-def AddCommonSystematics(cb):
-  
+def AddCommonSystematics(cb, year=None):
+  period = None
+  if year in ["2016","2017","2018"]:
+    period = 'Run2'
+  else:
+    period = 'Run3'
+
   signal = cb.cp().signals().process_set()
-  # rateParams
+
+  if period=='Run3':
+    cb.cp().AddSyst(cb,'lumi_13p6TeV','lnN', ch.SystMap()(1.010))
+  elif period=='Run2':
+    cb.cp().AddSyst(cb,'lumi_13TeV','lnN', ch.SystMap()(1.010))
 
   # Theory uncertainties: signal
   cb.cp().AddSyst(cb,
                   'pdf_Higgs_qqbar', 'lnN', ch.SystMap('process')
-                  (['ZH_hbb','ZH_hcc'],1.016)
-                  (['WH_hbb','WH_hcc'],1.019))
+                  (['ZH_hbb','ZH_hcc'], 1.016)
+                  (['WH_hbb','WH_hcc'], 1.019))
   
   cb.cp().process(['ggZH_hbb','ggZH_hcc']).AddSyst(cb,'pdf_Higgs_gg', 'lnN', ch.SystMap()(1.024))
-  
   cb.cp().process(['ggZH_hbb','ggZH_hcc']).AddSyst(cb,'QCDscale_ggZH', 'lnN',ch.SystMap()((1.251,0.811)))
   
   cb.cp().AddSyst(cb,'QCDscale_VH', 'lnN', ch.SystMap('process') 
@@ -20,4 +28,118 @@ def AddCommonSystematics(cb):
                   (['WH_hbb','WH_hcc'], (1.005,0.993)))
 
 
-  cb.cp().AddSyst(cb,'lumi_13TeV','lnN', ch.SystMap()(1.010))
+  cb.cp().process(['ZH_hcc','WH_hcc','ggZH_hcc']).AddSyst(cb,'BR_hcc', 'lnN', ch.SystMap()((1.05,0.97)))
+  cb.cp().process(['ZH_hbb','WH_hbb','ggZH_hbb']).AddSyst(cb,'BR_hbb', 'lnN', ch.SystMap()(1.005))
+  # Hbb branching ratio as measured by CMS: 
+  #cb.cp().process(['ZH_hbb','WH_hbb','ggZH_hbb']).AddSyst(cb,'BR_hbb', 'lnN', ch.SystMap()(1.20))
+  
+  #NLO EWK pt(V) correction to the VH and ggZH processes
+  cb.cp().AddSyst(cb,
+                  'CMS_vhcc_boost_EWK', 'lnN', ch.SystMap('channel','process') 
+                  (['Zee','Zmm'],['ZH_hcc','ggZH_hcc','ZH_hbb','ggZH_hbb'], 1.02)
+                  (['Znn'],['ZH_hcc','WH_hcc','ggZH_hcc','ZH_hbb','WH_hbb','ggZH_hbb'],1.02)
+                  (['Wen','Wmn'],['WH_hcc','ZH_hcc','WH_hbb','ZH_hbb'],1.02)) 
+
+  # Measured cross section uncertainties because we don't have SF:
+  #cb.cp().process(['VVother','VZcc']).AddSyst(cb,'CMS_vhcc_VV', 'lnN', ch.SystMap()(1.05)) 
+  cb.cp().process(['s_Top']).AddSyst(cb,'CMS_vhcc_ST', 'lnN', ch.SystMap()(1.15)) 
+
+  # Uncertainty on di-boson NNLO reweighting 
+  #cb.cp().process(['VVother','VZcc']).AddSyst(cb, 'CMS_VV_NNLOWeights_13TeV', 'shape', ch.SystMap()(1.0))
+
+  # Theoretical PDF uncertainties
+  cb.cp().process(['ggZH_hbb','ggZH_hcc']).AddSyst(cb,'CMS_LHE_pdf_ggZH', 'lnN', ch.SystMap()(1.023)) 
+  cb.cp().process(['ZH_hbb','ZH_hcc']).AddSyst(cb,'CMS_LHE_pdf_ZH', 'lnN', ch.SystMap()(1.018)) 
+  cb.cp().process(['WH_hbb','WH_hcc']).AddSyst(cb,'CMS_LHE_pdf_WH', 'lnN', ch.SystMap()(1.018))
+  cb.cp().process(['TT']).AddSyst(cb,'CMS_LHE_pdf_TT', 'lnN', ch.SystMap()(1.0265))
+  cb.cp().process(['s_Top']).AddSyst(cb,'CMS_LHE_pdf_ST', 'lnN', ch.SystMap()(1.0288))
+  cb.cp().process(['Zj_ll']).AddSyst(cb,'CMS_LHE_pdf_Zj_ll', 'lnN', ch.SystMap()(1.027))
+  cb.cp().process(['Zj_bj']).AddSyst(cb,'CMS_LHE_pdf_Zj_bj', 'lnN', ch.SystMap()(1.027))
+  cb.cp().process(['Zj_cj']).AddSyst(cb,'CMS_LHE_pdf_Zj_cj', 'lnN', ch.SystMap()(1.027))
+  cb.cp().process(['Wj_ll']).AddSyst(cb,'CMS_LHE_pdf_Wj_ll', 'lnN', ch.SystMap()(1.027))
+  cb.cp().process(['Wj_bj']).AddSyst(cb,'CMS_LHE_pdf_Wj_bj', 'lnN', ch.SystMap()(1.027))
+  cb.cp().process(['Wj_cj']).AddSyst(cb,'CMS_LHE_pdf_Wj_cj', 'lnN', ch.SystMap()(1.027))
+  #cb.cp().process(['VZcc']).AddSyst(cb,'CMS_LHE_pdf_VZcc', 'lnN', ch.SystMap()(1.015))
+  #cb.cp().process(['VVother']).AddSyst(cb,'CMS_LHE_pdf_VVother', 'lnN', ch.SystMap()(1.0135))
+
+  
+
+  
+  # Theoretical Renormalization and Factorization scale uncertainties
+  ### Shapes are not implemented yet. Need input from coffea
+  ### cb.cp().process(['ZH_hbb','ZH_hcc']).AddSyst(cb,'CMS_LHE_weights_scale_muR_ZH','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['WH_hbb','WH_hcc']).AddSyst(cb,'CMS_LHE_weights_scale_muR_WH','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['ggZH_hbb','ggZH_hcc']).AddSyst(cb,'CMS_LHE_weights_scale_muR_ggZH','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['ZH_hbb','ZH_hcc']).AddSyst(cb,'CMS_LHE_weights_scale_muF_ZH','shape',ch.SystMap()(1.0))
+  ### b.cp().process(['WH_hbb','WH_hcc']).AddSyst(cb,'CMS_LHE_weights_scale_muF_WH','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['ggZH_hbb','ggZH_hcc']).AddSyst(cb,'CMS_LHE_weights_scale_muF_ggZH','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Zj_ll']).AddSyst(cb,'CMS_LHE_weights_scale_muR_Zj_ll','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Zj_ll']).AddSyst(cb,'CMS_LHE_weights_scale_muF_Zj_ll','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Zj_bj']).AddSyst(cb,'CMS_LHE_weights_scale_muR_Zj_bj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Zj_bj']).AddSyst(cb,'CMS_LHE_weights_scale_muF_Zj_bj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Zj_cj']).AddSyst(cb,'CMS_LHE_weights_scale_muR_Zj_cj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Zj_cj']).AddSyst(cb,'CMS_LHE_weights_scale_muF_Zj_cj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Wj_ll']).AddSyst(cb,'CMS_LHE_weights_scale_muR_Wj_ll','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Wj_ll']).AddSyst(cb,'CMS_LHE_weights_scale_muF_Wj_ll','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Wj_bj']).AddSyst(cb,'CMS_LHE_weights_scale_muR_Wj_bj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Wj_bj']).AddSyst(cb,'CMS_LHE_weights_scale_muF_Wj_bj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Wj_cj']).AddSyst(cb,'CMS_LHE_weights_scale_muR_Wj_cj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['Wj_cj']).AddSyst(cb,'CMS_LHE_weights_scale_muF_Wj_cj','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['TT']).AddSyst(cb,'CMS_LHE_weights_scale_muR_TT','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['TT']).AddSyst(cb,'CMS_LHE_weights_scale_muF_TT','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['s_Top']).AddSyst(cb,'CMS_LHE_weights_scale_muR_ST','shape',ch.SystMap()(1.0))
+  ### cb.cp().process(['s_Top']).AddSyst(cb,'CMS_LHE_weights_scale_muF_ST','shape',ch.SystMap()(1.0))
+  ### #cb.cp().process(['VVother']).AddSyst(cb,'CMS_LHE_weights_scale_muR_VVother','shape',ch.SystMap()(1.0))
+  ### #cb.cp().process(['VZcc']).AddSyst(cb,'CMS_LHE_weights_scale_muR_VZcc','shape',ch.SystMap()(1.0))
+  ### #cb.cp().process(['VVother']).AddSyst(cb,'CMS_LHE_weights_scale_muF_VVother','shape',ch.SystMap()(1.0))
+  ### #cb.cp().process(['VZcc']).AddSyst(cb,'CMS_LHE_weights_scale_muF_VZcc','shape',ch.SystMap()(1.0))
+
+
+  ### RATEPARAMS (aka SCALE FACTORS) for TT, Z+Jets and W+jets processes
+  
+  # TT proc in 2L channel
+  cb.cp().channel(['Zee','Zmm']).process(['TT']).AddSyst(cb,
+     'SF_TT_2L_'+year, 'rateParam', ch.SystMap('bin_id')
+     ([1,3,4,5,6],  1.0))
+
+  # Zj_ll 2L
+  cb.cp().channel(['Zee','Zmm']).process(['Zj_ll']).AddSyst(cb,
+     'SF_Zj_ll_2L_'+year, 'rateParam', ch.SystMap('bin_id')
+     ([1,3,4,5,6],  1.0))
+
+  # Zj_bj 2L
+  cb.cp().channel(['Zee','Zmm']).process(['Zj_bj']).AddSyst(cb,
+     'SF_Zj_bj_2L_'+year, 'rateParam', ch.SystMap('bin_id')
+     ([1,3,4,5,6],1.0))
+
+  # Zj_cj 2L
+  cb.cp().channel(['Zee','Zmm']).process(['Zj_cj']).AddSyst(cb,
+     'SF_Zj_cj_2L_'+year, 'rateParam', ch.SystMap('bin_id')
+     ([1,3,4,5,6],1.0))
+
+
+  # Uncertainties specific to data-taking period:
+  if year == '2016':
+    cb.cp().process(['Wj_bj']).AddSyst(cb,'Norm_Wj_bj_2016', 'lnN', ch.SystMap()(1.50))
+  elif year == '2017':
+    cb.cp().process(['Wj_bj']).AddSyst(cb,'Norm_Wj_bj_2017', 'lnN', ch.SystMap()(1.50))
+  elif year == '2018':
+    cb.cp().process(['Wj_bj']).AddSyst(cb,'Norm_Wj_bj_2018', 'lnN', ch.SystMap()(1.50))
+
+  elif year in ['2022_preEE','2022_postEE','2023_preBPix','2023_postBPix']:
+    cb.cp().process(['Wj_bj']).AddSyst(cb,'Norm_Wj_bj_'+year, 'lnN', ch.SystMap()(1.50))    
+    
+
+  
+  #========= Lepton efficiencies
+  if period=='Run2':
+    cb.cp().channel(['Wmn']).AddSyst(cb,'CMS_vhcc_eff_m_Wln_13TeV_'+year,'lnN',ch.SystMap()(1.02))
+    cb.cp().channel(['Wen']).AddSyst(cb,'CMS_vhcc_eff_e_Wln_13TeV_'+year,'lnN',ch.SystMap()(1.02))
+    cb.cp().channel(['Zmm']).AddSyst(cb,'CMS_vhcc_eff_m_Zll_13TeV_'+year,'lnN',ch.SystMap()(1.04))
+    cb.cp().channel(['Zee']).AddSyst(cb,'CMS_vhcc_eff_e_Zll_13TeV_'+year,'lnN',ch.SystMap()(1.04))
+
+  elif period=='Run3':
+    cb.cp().channel(['Wmn']).AddSyst(cb,'CMS_vhcc_eff_m_Wln_13p6TeV_'+year,'lnN',ch.SystMap()(1.02))
+    cb.cp().channel(['Wen']).AddSyst(cb,'CMS_vhcc_eff_e_Wln_13p6TeV_'+year,'lnN',ch.SystMap()(1.02))
+    cb.cp().channel(['Zmm']).AddSyst(cb,'CMS_vhcc_eff_m_Zll_13p6TeV_'+year,'lnN',ch.SystMap()(1.04))
+    cb.cp().channel(['Zee']).AddSyst(cb,'CMS_vhcc_eff_e_Zll_13p6TeV_'+year,'lnN',ch.SystMap()(1.04))
--- scripts/vhcc_cards.py
+++ scripts/vhcc_cards.py
@@ -16,9 +16,31 @@ parser.add_argument(
 parser.add_argument(
     '-o','--output_folder', default='vhcc_Run3', help="""Subdirectory of ./output/ where the cards are written out to""")
 parser.add_argument(
-    '-y','--year', default='2022_postEE', help="""Yea/Era to produce datacards""")
+    '-y','--year', default='2022_postEE', help="""Year/Era to produce datacards""")
+parser.add_argument(
+    '--bbb', default=1, type=int, help="""Sets the type of bin-by-bin uncertainty. 0: no bin-by-bins, 1: autoMCStats""")
 
 args = parser.parse_args()
+
+
+def matching_proc(p,s):
+    return ((p.bin()==s.bin()) and (p.process()==s.process()) and (p.signal()==s.signal()) 
+            and (p.analysis()==s.analysis()) and  (p.era()==s.era()) 
+            and (p.channel()==s.channel()) and (p.bin_id()==s.bin_id()) and (p.mass()==s.mass()))
+
+def drop_zero_procs(chob, proc):
+    null_yield = not (proc.rate() > 0.)
+    if(null_yield):
+        chob.FilterSysts(lambda sys: matching_proc(proc,sys)) 
+    return null_yield
+
+def drop_zero_systs(syst):
+    null_yield = (not (syst.value_u() > 0. and syst.value_d()>0.) ) and syst.type() in 'shape'
+    if(null_yield):
+        print('Dropping systematic ',syst.name(),' for region ', syst.bin(), ' ,process ', syst.process(), '. up norm is ', syst.value_u() , ' and down norm is ', syst.value_d())
+        #chob.FilterSysts(lambda sys: matching_proc(proc,sys)) 
+    return null_yield
+
 
     
 def createCards():
@@ -45,12 +67,11 @@ def createCards():
     if year not in ["2016","2017","2018",'2022_preEE','2022_postEE','2023_preBPix','2023_postBPix']:
         print("Year ", year, " not supported!")
         sys.exit()
-        
-    
+
     bkg_procs = {
         #'Zmm' : ['ZH_hbb','ggZH_hbb','s_Top','TT','Zj_ll','Zj_bj','Zj_cj','VVother','VZcc'],
-        'Zmm' : ['TT','Zj_ll','WW','WZ','ZZ'],
-        'Zee' : ['TT','Zj_ll','WW','WZ','ZZ'],
+        'Zmm' : ['TT','Zj_ll','Zj_cj','Zj_bj','WW','WZ','ZZ'],
+        'Zee' : ['TT','Zj_ll','Zj_cj','Zj_bj','WW','WZ','ZZ'],
     }
     
     sig_procs = {
@@ -60,13 +81,13 @@ def createCards():
     
     cats = {
         'Zmm' : [
-            (1, 'Zmm_SR'), (2, 'Zmm_SR'), (3, 'Zmm_CR_HF'), (4,'Zmm_CR_LF'), (5,'Zmm_CR_TT'), (6,'Zmm_CR_CC')
+            (1, 'Zmm_SR'), (3, 'Zmm_CR_HF'), (4,'Zmm_CR_LF'), (5,'Zmm_CR_TT'), (6,'Zmm_CR_CC')
             #(1, 'SR_high_Zmm'), (2, 'SR_low_Zmm'), (3, 'Zlf_high_Zmm'), (4,'Zlf_low_Zmm'),
             #(5, 'Zhf_high_Zee'), (6, 'Zhf_low_Zee'),
             #(7,'ttbar_high_Zee'), (8,'ttbar_low_Zee'),(9,'Zcc_high_Zee'), (10,'Zcc_low_Zee')
         ],
         'Zee' : [
-            (1, 'Zee_SR'), (2, 'Zee_SR'), (3, 'Zee_CR_HF'), (4,'Zee_CR_LF'), (5,'Zee_CR_TT'), (6,'Zee_CR_CC')
+            (1, 'Zee_SR'), (3, 'Zee_CR_HF'), (4,'Zee_CR_LF'), (5,'Zee_CR_TT'), (6,'Zee_CR_CC')
             #(1, 'SR_high_Zmm'), (2, 'SR_low_Zmm'), (3, 'Zlf_high_Zmm'), (4,'Zlf_low_Zmm'),
             #(5, 'Zhf_high_Zee'), (6, 'Zhf_low_Zee'),
             #(7,'ttbar_high_Zee'), (8,'ttbar_low_Zee'),(9,'Zcc_high_Zee'), (10,'Zcc_low_Zee')
@@ -74,11 +95,16 @@ def createCards():
     }
     for chn in chns:
         cb.AddObservations( ['*'], ['vhcc'], ['13p6TeV'], [chn], cats[chn])
-        cb.AddProcesses( ['*'], ['vhcc'], ['13p6TeV'], [chn], bkg_procs[chn], cats[chn], False)
         cb.AddProcesses( ['*'], ['vhcc'], ['13p6TeV'], [chn], sig_procs[chn], cats[chn], True)
+        cb.AddProcesses( ['*'], ['vhcc'], ['13p6TeV'], [chn], bkg_procs[chn], cats[chn], False)
     
-    systs.AddCommonSystematics(cb)
+    systs.AddCommonSystematics(cb, year)
     
+
+    if args.bbb==0:
+        cb.AddDatacardLineAtEnd("* autoMCStats -1")
+    elif args.bbb==1:
+        cb.AddDatacardLineAtEnd("* autoMCStats 0")
     
     for chn in chns:
         if chn in ['Zll','Zmm','Zee']:
@@ -88,18 +114,27 @@ def createCards():
         elif chn in ['Znn']:
             input_root_file = "./vhcc_shapes_0L.root"
         
-        cb.cp().channel([chn]).backgrounds().bin_id([1,2,3,4,5,6]).ExtractShapes(
-            input_root_file, year+'_$BIN/$PROCESS_Shape_nominal', 'Shape_$BIN_$PROCESS_$SYSTEMATIC')
-        cb.cp().channel([chn]).signals().bin_id([1,2,3,4,5,6]).ExtractShapes(
+        cb.cp().channel([chn]).signals().bin_id([1,3,4,5,6]).ExtractShapes(
             input_root_file, year+'_$BIN/$PROCESS_Shape_nominal', 'Shape_$BIN_$PROCESS_$SYSTEMATIC')    
+        cb.cp().channel([chn]).backgrounds().bin_id([1,3,4,5,6]).ExtractShapes(
+            input_root_file, year+'_$BIN/$PROCESS_Shape_nominal', 'Shape_$BIN_$PROCESS_$SYSTEMATIC')
         
-    
+
+        
+    print('binning in TT CRs: [0., 1.0] for Zll channels')
+    cb.cp().channel(['Zee','Zmm']).bin_id([7]).VariableRebin([0., 1.0])
+        
     ch.SetStandardBinNames(cb)
-    
-    
+       
     
     writer=ch.CardWriter("output/" + args.output_folder + "_" + year + "/$TAG/$BIN_"+year+".txt",
                          "output/" + args.output_folder + "_" + year +"/$TAG/shapes_$BIN_"+year+".root")
+
+
+
+    cb.FilterProcs(lambda x: drop_zero_procs(cb,x))
+    cb.FilterSysts(lambda x: drop_zero_systs(x))
+
     
     writer.SetWildcardMasses([])
     writer.SetVerbosity(0)
